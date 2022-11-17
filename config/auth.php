c1583aa36fa88599d5489405707ae604f452d2c3
Vincent Chi <song374561@chivincent.net>
2022-11-17 23:22:33 +0800

    feat: use kratos for authentication

--- config/auth.php
+++ config/auth.php
@@ -40,6 +40,11 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+
+        'kratos' => [
+            'driver' => 'kratos',
+            'provider' => 'kratos',
+        ],
     ],
 
     /*
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::middleware('auth:kratos')
+    ->get('/user', fn (Request $request) => response()->json($request->user()));
